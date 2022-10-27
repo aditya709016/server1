ca2afd4270b5ea1efa02deee020fb3586942faab
aditya709016 <adityaraj.as709@gmail.com>
2022-10-27 22:10:43 +0530

    task complete

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-const express= require("express");
-const app = express()
+const express = require("express");
 
-app.get("/",function(req,res){
-    var name =""
-    res.send(`your name is ${name}`)
-})
-app.get("/toUppercase",function(req,res){
-    console.log(req.query);
-    var name = req.query.name;
-    name =name.toUpperCase()
-    res.send(`my name is ${name},my fav movie is ${movie}`)
-})
-app.get("/route1/:p",(req,res)=>{
-    console.log(req.params);
-    res.send("OK")
-})
+const app = express();
 
+app.get("/", function (req, res) {
+  var name = req.query.name;
+  var movie = req.query.movie;
+  res.send("my name is ${name}, fav movie is ${movie}");
+});
 
-app.get("/calculate/:operation",(req,res) => 
-{
-  const opr = req.params.operation
-  if(opr == "add")
-  {
-  const num1 = Number(req.query.num1)
-  const num2 = Number(req.query.num2)
-  const result = num1 + num2
-  res.send(result + "")
+//demonstration of param
+app.get("/route1/:parameter", function (req, res) {
+  console.log(req.params);
+  res.send("OK");
+});
+
+app.get("/calculate/:operation", function (req, res) {
+  const opr = req.params.operation;
+  console.log(req.params);
+  console.log(req.query);
+
+  if (opr == "add") {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+    const result = num1 + num2;
+    res.send(result + "");
+  } else if (opr == "sub") {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+    const result = num1 - num2;
+    res.send(result + "");
+  } else if (opr == "mul") {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+    const result = num1 * num2;
+    res.send(result + "");
+  } else if (opr == "div") {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+    const result = num1 / num2;
+    res.send(result + "");
+  } else {
+    res.send("Operation not found...");
   }
-  else {
-    res.send("operation not found");
-  }
-})
-
-
-app.listen(8000, function(){
-    console.log("Server is up");
-})
+});
+app.listen(8000, function () {
+  console.log("Server is up.");
+});
