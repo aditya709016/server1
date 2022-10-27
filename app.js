a7cdd9c69dc453e7c6947ec744e639bee353502f
aditya709016 <adityaraj.as709@gmail.com>
2022-10-27 21:44:39 +0530

    Simple_Server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=reqire("express");
+const app=express();
+
+app.get("/", function(req, res){
+    res.send("this is the first server");
+})
+
+app.listen(80, function(){
+    console.log("the server has started on port 80");
+})
